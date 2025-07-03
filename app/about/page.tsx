86f2817d0ea0e8863532653c11d0232a65fbf358
sarbeswar_cloudium <sarbeswar.sahoo@cloudium.io>
2025-07-03 15:32:03 +0530

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/header";
+
+export default function About() {
+    return (
+        <>
+        <Header/>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <div className="">
+                    <h1>About Page</h1>
+                </div>
+            </main>
+        </div>
+        </>
+    )
+}
