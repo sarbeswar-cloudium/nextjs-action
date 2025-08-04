6f0f1a256130eb45e58fa990d295357f8c649dbc
sarbeswar_cloudium <sarbeswar.sahoo@cloudium.io>
2025-08-04 17:10:30 +0530

    added login and unauthenticated pages

---
+++ app/(auth)/unauthenticated/page.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/header';
+
+const Unauthenticated = () => {
+    return (
+        <>
+        <Header />
+        <div className="">
+            <h1>Unauthenticated</h1>
+        </div>
+        </>
+    )
+}
+
+export default Unauthenticated;
